c7b55c4b03305457e33c17939c8779da09446dcf
khach <khachgevorgyan94@gmail.com>
2023-12-15 17:51:30 +0400

    Project started

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/header/header";
+
+class App extends React.Component{
+
+    render() {
+        return <div>
+            <Header/>
+
+
+            Hello word
+
+
+        </div>
+    }
+
+}
+
+export default App
+
