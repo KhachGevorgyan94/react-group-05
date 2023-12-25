5cb4d8f3e53bbfb8bdec613ef5609cad146e28d9
khach <khachgevorgyan94@gmail.com>
2023-12-25 21:28:29 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
 import React from "react";
-import Header from "./components/header/header";
-import ProductCard from "./components/productCard";
-import natureImage from './assets/imnages/free-nature-images.jpg'
-import TextBox from "./components/TextBox";
+import Text from "./components/Text";
+import Modal from "./components/modal";
 
 class App extends React.Component {
 
-    users = [
-        {
-            title: 'test',
-            isCheck: false,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg',
-            image: natureImage
-        },
-        {
-            title: 'test',
-            isCheck: false,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg'
-        },
-        {
-            title: 'Khachik',
-            isCheck: true,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg'
-        },
-        {
-            title: 'test',
-            isCheck: false,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg'
-        },
-        {
-            title: 'test',
-            isCheck: false,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg'
-        },
-        {
-            title: 'test',
-            isCheck: false,
-            description: 'qweqweqwe',
-            subTitle: '23qelrktjewlkrjtg'
-        }
-    ]
+    state = {
+        isOpenModal: false
+    }
 
+    openModal = ()=>{
+        this.setState({isOpenModal:true})
+    }
+    closeModal = ()=>{
+        this.setState({isOpenModal:false})
+    }
 
     render() {
         return <div className={'G-flex-column G-center gap-15'}>
-            {/*{this.users.map((item, index) => {*/}
-            {/*    return <ProductCard*/}
-            {/*        key={'product-card' + index}*/}
-            {/*        title={item.title}*/}
-            {/*        image={item.image}*/}
-            {/*        isCheck={item.isCheck}*/}
-            {/*        description={item.description}*/}
-            {/*        subTitle={item.subTitle}/>*/}
-            {/*})}*/}
+            <button onClick={this.openModal}>open modal</button>
+            {this.state.isOpenModal ? <Modal close={this.closeModal}>
+                as;lkjdfha sljkhdfa;k h
+            </Modal> : null}
 
-            <TextBox title={'asdasd'} description={'asdasd'}/>
-            <TextBox title={'asdasd'} description={'asdasd'}/>
-            <TextBox title={'asdasd'} description={'asdasd'}/>
+            
         </div>
     }
 
