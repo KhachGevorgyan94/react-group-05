74cd3bc338eccc17008ab9a63284b3908cc38789
khach <khachgevorgyan94@gmail.com>
2024-02-06 01:23:20 +0400

    test

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,6 +3,11 @@ import './style.scss'
 import axios from "axios";
 import {BaseURL} from "../../routers/routers";
 
+/**
+ * Contact component represents a contact page that displays a to-do list fetched from an API.
+ *
+ * @returns {React.Component} The Contact component
+ */
 export const Contact = () => {
 
     const [toDoList, setToDoList] = useState([])
@@ -51,6 +56,15 @@ export const Contact = () => {
 }
 
 
+/**
+ * Generates a To-Do card element based on the provided item.
+ *
+ * @param {Object} item - The item object representing a To-Do.
+ * @param {string} item.title - The title of the To-Do.
+ * @param {boolean} item.completed - Indicates whether the To-Do is completed or not.
+ *
+ * @return {ReactElement} - The generated To-Do card element.
+ */
 function ToDoCard({item}) {
     return <div className={`to-do-card ${item.completed ? 'completed' : ''}`}>
         <p>{item.title}</p>
